08338c8f14373560010088494456c7a2e5f427ac
kimjihyunKR <kkkk6810@on.mjc.ac.kr>
2020-12-09 13:52:44 +0900

    db module test

--- test/userTest.js
+++ test/userTest.js
@@ -1,11 +1,5 @@
 var mysql = require('mysql');
-var db = mysql.createConnection({
-  host     : 'localhost',
-  port     : 3306 ,
-  user     : 'root',
-  password : '1234',
-  database : 'moumzip'
-});
+const db = require('../config/db');
  
 db.connect();
 
