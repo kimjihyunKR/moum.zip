c2cd9fb36842b762debff8fe8bee0f4876adbc98
kimjihyunKR <kkkk6810@on.mjc.ac.kr>
2020-12-10 01:18:42 +0900

    users form

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('../config/db');
+
+const router = express.Router();
+
+router.post('/:id/profile', async (req, res, next) => {
+
+});
+
+module.exports = router;
