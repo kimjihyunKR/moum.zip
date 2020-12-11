a38383661eab42b920c16d381aa340a1d6da7255
kimjihyunKR <kkkk6810@on.mjc.ac.kr>
2020-12-12 05:39:57 +0900

    mapper

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,21 @@ const express = require('express');
 const passport = require('passport');
 const db = require('../config/db');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const users = require('../mapper/users');
+
 const router = express.Router();
 
 router.post('/join', isNotLoggedIn ,async (req, res, next) => {
   const { id, password, name } = req.body;
   try {
-    //const exUser = await User.findOne({ where: { email } });
-    let sql = 'SELECT user_id FROM users WHERE user_id=?';
-    // db.connect();
-    db.query(sql,[id], function (error, results, fields) {
+    db.query( users.selectByUserid ,[id], function (error, results, fields) {
       if (error) throw error;
       if(results[0]){
         return res.send('이미 있는 아이디');
       }
     });
     
-    let sql2 = 'INSERT INTO users (user_id, password, name, nickname) VALUES (?,?,?,?);';
-    db.query(sql2,[id, password, name,name], (error, results, fields) => {
+    db.query( users.insert ,[id, password, name,name], (error, results, fields) => {
       if (error) throw error;
       console.log('가입완료');
       res.redirect('/');
